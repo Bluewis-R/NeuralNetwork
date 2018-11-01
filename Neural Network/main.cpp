4934d1baf4865f07400bc1cc8368f36b58b48196
Lewis Remington <s5063237@bournemouth.ac.uk>
2018-11-01 13:56:37 +0000

    Added a bit more from the latest lab

--- Neural Network/main.cpp
+++ Neural Network/main.cpp
@@ -146,36 +146,43 @@ int signFunction(int x, float _theta, float _delta)
 	}
 }
 
-/*
+
 float SigmoidFunction(float _x, float _theta)
 {
-return (1 / (1 + pow(e, -(_x - _theta))));
+	return (1 / (1 + pow(exp(1), -(_x - _theta))));
 }
-*/
+
 
 
 void multiNeuron()
 {
-	//initiating variables
+	//	initiating variables
 	int itterations = 100000;
-	
-	float weight1_3 = 0.5f;
-	float weight1_4 = 0.9f;
-	float weight3_5 = -1.2f;
-	float weight2_3 = 0.4f;
-	float weight2_4 = 1.0f;
-	float weight4_5 = 1.1f;
+	//	This is wastfull but easy to do for now,
+	//	in future this can be replaced with something more efficient
+	float weights[4][5];
+	float weightsCurrent[5][6];
 
-	float theta3 = 0.8f;
-	float theta4 = 0.4f;
-	float theta5 = 0.3f;
+	weights[1][3] = 0.5f;
+	weights[1][4] = 0.9f;
+	weights[3][5] = -1.2f;
+	weights[2][3] = 0.4f;
+	weights[2][4] = 1.0f;
+	weights[4][5] = 1.1f;
+	
+
+	float theta[6];
+	theta[3] = 0.8f;
+	theta[4] = 0.4f;
+	theta[5] = 0.3f;
 	float alpha = 0.1f;
 
 	int x[3][4];
-	int Yd5[4];
-	float X[5];
-	float Y[5];
-	float e[5];
+	int Yd5[5];
+	float X[6];
+	float Y[6];
+	float error[6];
+	float delta[6];
 
 	x[1][1] = 1;
 	x[1][2] = 0;
@@ -190,6 +197,8 @@ void multiNeuron()
 	Yd5[3] = 1;
 	Yd5[4] = 0;
 
+	float tX[6];
+	float tY[6];
 
 	//code
 	int p = 0;
@@ -200,22 +209,49 @@ void multiNeuron()
 		{
 			std::cout << p << std::endl;
 			//neuron 3
-			X[3] = (x[1][i] * weight1_3) + (x[2][i] * weight2_3);
-			Y[3] = 1 / ( 1 + pow(exp(1), -(X[3] - theta3) ));
+			X[3] = (x[1][i] * weights[1][3]) + (x[2][i] * weights[2][3]);
+			Y[3] = 1 / ( 1 + pow(exp(1), -(X[3] - theta[3]) ));
 
 			//neuron 4
-			X[4] = (x[1][i] * weight1_4) + (x[2][i] * weight2_4);
-			Y[4] = 1 / (1 + pow(exp(1), -(X[4] - theta4)));
+			X[4] = (x[1][i] * weights[1][4]) + (x[2][i] * weights[2][4]);
+			Y[4] = 1 / (1 + pow(exp(1), -(X[4] - theta[4])));
 
 			//neuron 5
-			X[5] = (x[1][i] * weight3_5) + (x[2][i] * weight4_5);
-			Y[5] = 1 / (1 + pow(exp(1), -(X[5] - theta5)));
+			X[5] = (x[1][i] * weights[3][5]) + (x[2][i] * weights[4][5]);
+			Y[5] = 1 / (1 + pow(exp(1), -(X[5] - theta[5])));
 
 
 			//	Back propergating the error
 			// N5 error
-			e[5] = Yd5[i] - Y[i];
-			Delta = 
+			error[5] = Yd5[i] - Y[i];
+			delta[5] = Y[5] * (1 - Y[5])*error[5];
+			weightsCurrent[3][5] = weights[3][5];
+			weightsCurrent[4][5] = weights[4][5];
+			weights[3][5] += alpha * Y[3] * delta[5];
+			weights[4][5] += alpha * Y[4] * delta[5];
+			theta[5] += alpha * (-1)*delta[5];
+
+			// N3 error
+			error[3] = weightsCurrent[3][5] * error[5];
+			delta[3] = Y[3] * (1 - Y[3])*delta[5]*weightsCurrent[3][5];
+			weights[1][3] += alpha * Y[i] * delta[3];
+			weights[2][3] += alpha * Y[i] * delta[3];
+			theta[3] += alpha * (-1)*delta[3];
+
+			// N4 error
+			error[4] = weightsCurrent[4][5] * error[5];
+			delta[4] = Y[4] * (1 - Y[4])*delta[5] * weightsCurrent[4][5];
+			weights[1][4] += alpha * Y[i] * delta[4];
+			weights[2][4] += alpha * Y[i] * delta[4];
+			theta[4] += alpha * (-1)*delta[4];
+
+			//	summing errors
+			tX[3] = (x[1][i] * weights[1][3]) + (x[2][i] * weights[2][3]);
+			tY[3] = SigmoidFunction(tX[3], theta[3]);
+			tX[4] = (x[1][i] * weights[1][4]) + (x[2][i] * weights[2][4]);
+			tY[4] = SigmoidFunction(tX[4], theta[4]);
+			tX[5] = (ty[3] * weights[1][4]) + (x[2][i] * weights[2][4]);
+			tY[5] = SigmoidFunction(tX[4], theta[4]);
 		}
 	}
 		
